7584bcba2f9df60cc1e7b26d9437e3efad582ff8
Elchin Guliyev <elcnqlyv24@gmail.com>
2026-09-15 10:04:47 +0400

    Enhance bit manipulation utility with set, clear, and toggle bit options

--- bit-manipulation-utility-main.c
+++ bit-manipulation-utility-main.c
@@ -27,7 +27,12 @@ int main()
         printf("\n1. Set a value\n");
         printf("2. Show value in decimal\n");
         printf("3. Show value in binary\n");
-        printf("4. Exit\n");
+        printf("4. Set a bit\n");
+        printf("5. Clear a bit\n");
+        printf("6. Toggle a bit\n");
+        printf("7. Exit\n");
+
+
 
         printf("Choose: ");
         scanf("%d", &choice);
@@ -35,23 +40,85 @@ int main()
         switch (choice)
         {
         case 1:
+        {
             printf("Enter the value you want: ");
             scanf("%u", &value);
             break;
+        }
         case 2:
-            printf("The value is %u in decimal.", value);
+        {
+            printf("The value is %u in decimal.\n", value);
             break;
+        }
         case 3:
+        {
             printBinary(value);
             break;
+        }
         case 4:
+        {   
+            int bit;
+
+            printf("Which bit do you want to set? ");
+            scanf("%d", &bit);
+
+            if (bit < 0 || bit > 31)
+            {
+                printf("Bit must be between 0 and 31. \n");
+                break;
+            }
+            
+            value|= (1U << bit);
+
+            printf("Bit %d was set. \n", bit);
+            break;
+        }     
+        case 5:
+        {
+            int bit;
+         
+            printf("Which bit do you want to clear? ");
+            scanf("%d", &bit);
+
+            if (bit<0 || bit>31)
+            {
+                printf("Bit must be between 0 and 31. \n");
+                break;
+            }
+            
+
+            value &= ~(1U << bit);
+
+            printf("Bit %d was cleared. \n", bit);
+            break;
+        }
+        case 6:
+        {
+            int bit; 
+            printf("Which bit do you want to toggle?");
+            scanf("%d", &bit);
+
+            if (bit<0 || bit >31)
+            {
+                printf("Bit must be between 0 and 31. \n");
+                break;
+            }
+            value ^= (1U << bit);
+
+            printf("Bit %d was toggled. \n", bit);
+            break;
+        }
+        case 7:
+        {
             printf("Exiting...\n");
             break;
+        }
+
         default:
             printf("Invalid choice.\n");
         }
 
-    } while (choice != 4);
+    } while (choice != 7);
     
     return 0;
 }
